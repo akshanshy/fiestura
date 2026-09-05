8bd5fc6160f9f782da273f5aeeea420a379a1a57
akshanshy <akshanshy2@gmail.com>
2026-09-05 16:44:59 +0530

    Refactor package.json scripts, add launch and tasks configuration files, and create package-lock.json

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -51,7 +51,7 @@ router.post("/login", async (req, res) => {
    const token = jwt.sign(
   { 
     id: user._id,
-    role: user.role   // 🔥 ADD THIS
+    role: user.role 
   },
   process.env.JWT_SECRET,
   { expiresIn: "1d" }
